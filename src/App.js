bab001966a9434cbc32205963ce332865be6fdbd
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-06 19:41:37 +0200

    moved form to component with styling

--- src/App.js
+++ src/App.js
@@ -1,46 +1,18 @@
 import Header from "./Header";
+import Form from "./Form";
 
 function App() {
   return (
-    <>
+    <main className="main">
       <Header
         title={"$ KALKULATOR WALUT $"}
         subtitleText={"Kursy walut z dnia 24 lipca 2023 roku"}
       />
-      <main className="main">
-        <form>
-          <fieldset className="form__fieldset">
-            <legend className="form__legend">Wprowadź dane</legend>
-            <p>
-              <label>
-                <span className="form__labelText">Wpisz kwotę:</span>
-                <input type="number" name="amount" step="0.01" min="0.01" className="form__field"
-                  required />
-              </label>
-              <select name="currency-input" className="form__field form__field--secondary" defaultValue={"PLN"}>
-                <option>PLN</option>
-                <option>USD</option>
-                <option>EUR</option>
-              </select>
-            </p>
-            <p>
-              <label>
-                <span className="form__labelText">Wybierz walutę:</span>
-                <select name="currency-output" className="form__field" defaultValue={"USD"}>
-                  <option value="PLN">Złoty (PLN)</option>
-                  <option value="USD">Dolar amerykański (USD)</option>
-                  <option value="EUR">Euro (EUR)</option>
-                </select>
-              </label>
-            </p>
-            <button className="form__button">KONWERTUJ</button>
-          </fieldset>
-          <div className="form__resultContainer">
-            <p>Wynik:</p>
-          </div>
-        </form>
-      </main>
-    </>
+      <Form />
+      <div className="form__resultContainer">
+        <p>Wynik:</p>
+      </div>
+    </main >
   );
 }
 
---
+++ src/Form/index.js
@@ -0,0 +1,34 @@
+import './style.css'
+
+const Form = () => (
+    <form>
+        <fieldset className="form__fieldset">
+            <legend className="form__legend">Wprowadź dane</legend>
+            <p>
+                <label>
+                    <span className="form__labelText">Wpisz kwotę:</span>
+                    <input type="number" name="amount" step="0.01" min="0.01" className="form__field"
+                        required />
+                </label>
+                <select name="currency-input" className="form__field form__field--secondary" defaultValue={"PLN"}>
+                    <option>PLN</option>
+                    <option>USD</option>
+                    <option>EUR</option>
+                </select>
+            </p>
+            <p>
+                <label>
+                    <span className="form__labelText">Wybierz walutę:</span>
+                    <select name="currency-output" className="form__field" defaultValue={"USD"}>
+                        <option value="PLN">Złoty (PLN)</option>
+                        <option value="USD">Dolar amerykański (USD)</option>
+                        <option value="EUR">Euro (EUR)</option>
+                    </select>
+                </label>
+            </p>
+            <button className="form__button">KONWERTUJ</button>
+        </fieldset>
+    </form>
+);
+
+export default Form;
