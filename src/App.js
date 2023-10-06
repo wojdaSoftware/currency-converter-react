3a5649ea94aa099825e0d07a8558f54d0f1e33ee
Kacper Wojda <wojda.kacper@gmail.com>
2023-10-06 11:26:48 +0200

    removed js related classes

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
         <p className="header__paragraph">Kursy walut z dnia 24 lipca 2023 roku</p>
       </header>
       <main className="main">
-        <form className="js-form">
+        <form>
           <fieldset className="form__fieldset">
             <legend className="form__legend">Wprowadź dane</legend>
             <p>
               <label>
                 <span className="form__labelText">Wpisz kwotę:</span>
-                <input type="number" name="amount" step="0.01" min="0.01" className="form__field js-amount"
+                <input type="number" name="amount" step="0.01" min="0.01" className="form__field"
                   required />
               </label>
-              <select name="currency-input" className="form__field form__field--secondary js-currency-input">
+              <select name="currency-input" className="form__field form__field--secondary">
                 <option selected>PLN</option>
                 <option>USD</option>
                 <option>EUR</option>
@@ -24,7 +24,7 @@ function App() {
             <p>
               <label>
                 <span className="form__labelText">Wybierz walutę:</span> <select name="currency-output"
-                  className="form__field js-currency-output">
+                  className="form__field">
                   <option value="PLN">Złoty (PLN)</option>
                   <option value="USD" selected>Dolar amerykański (USD)</option>
                   <option value="EUR">Euro (EUR)</option>
@@ -34,7 +34,7 @@ function App() {
             <button className="form__button">KONWERTUJ</button>
           </fieldset>
           <div className="form__resultContainer">
-            <p className="js-result">Wynik:</p>
+            <p>Wynik:</p>
           </div>
         </form>
       </main>
