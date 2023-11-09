fd561543b3ed3c30e3104a573b4dbde7e7b77eda
Kacper Wojda <wojda.kacper@gmail.com>
2023-11-09 13:28:26 +0100

    functional clock

--- src/Clock/index.js
+++ src/Clock/index.js
@@ -1,9 +1,28 @@
 import './style.css'
+import { useEffect, useState } from 'react'
 
-const Clock = () => (
-    <div className="clockContainer">
-        placeholder text
-    </div>
-)
+const Clock = () => {
+    const [date, setDate] = useState(new Date());
+
+    const formatDate = (date) => {
+        return date.toLocaleString(undefined, {});
+    };
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId);
+        }
+    }, []);
+
+    return (
+        <div className="clockContainer">
+            Dziś jest {formatDate(date)}
+        </div>
+    )
+}
 
 export default Clock;
