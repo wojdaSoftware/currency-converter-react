022e8ee38785e68a808dd238720eadb8b7a4e0bc
Kacper Wojda <wojda.kacper@gmail.com>
2023-11-16 21:51:41 +0100

    created a GlobalStyle and a theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import Header from "./Header";
 import Form from "./Form";
 import Container from "./Content";
 import Clock from "./Clock";
+import background from './converter-background.jpg';
+
+const theme = {
+  breakpoint: 767,
+  colors: {
+    primary: "#646464",
+    secondary: "#cfcfcf",
+    ternary: "#fffd7c",
+  },
+}
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+    ::after,
+    ::before {
+      box-sizing: inherit;
+  }
+
+  body {
+    background-image: url(${background});
+    background-repeat: repeat-y;
+    background-size: cover;
+    font-family: 'Poppins', sans-serif;
+    margin: 0 20px;
+  }
+`;
 
 function App() {
   return (
-    <Container>
-      <Header aboveTitleContent={<Clock />} />
-      <Form />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Container>
+        <Header aboveTitleContent={<Clock />} />
+        <Form />
+      </Container>
+    </ThemeProvider>
   );
 }
 
