6727af3fadafcee2984450db0d441059a9e7c8be
Kacper Wojda <wojda.kacper@gmail.com>
2023-11-21 12:06:11 +0100

    moved styled-components to index.js

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,4 +1,15 @@
-import { Main } from './styled.js';
+import styled from "styled-components";
+
+const Main = styled.main`
+    margin: auto;
+    max-width: 600px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    align-content: center;
+    height: 100vh;
+`;
 
 const Container = ({children}) => (
     <Main>
