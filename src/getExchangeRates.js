e2ca390ff918c9c13e3eb4ccfa613c4ceef55426
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-04 20:32:06 +0100

    removed console logging

--- src/getExchangeRates.js
+++ src/getExchangeRates.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const getExchangeRates = () => {
     return axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_rycdtBwznBtiZdlOVMz0OLXrcBYp1XIJV2EZYJhc&currencies=EUR%2CUSD%2CGBP&base_currency=PLN')
         .then(response => {
-            console.log(response);
             return response.data;
         })
         .catch(error => {
