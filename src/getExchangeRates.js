5852c5edbfef4b9608a47be4a1be8e4c69100d57
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-04 19:45:36 +0100

    functional exchange rate fetching from API

--- src/getExchangeRates.js
+++ src/getExchangeRates.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const getExchangeRates = async () => {
-    try {
-        const exchangeRates = await axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_rycdtBwznBtiZdlOVMz0OLXrcBYp1XIJV2EZYJhc');
-        console.log(exchangeRates);
-        return exchangeRates;
-    } catch (error) {
-        console.error(error);
-    }
+const getExchangeRates = () => {
+    return axios.get('https://api.currencyapi.com/v3/latest?apikey=cur_live_rycdtBwznBtiZdlOVMz0OLXrcBYp1XIJV2EZYJhc&currencies=EUR%2CUSD%2CGBP&base_currency=PLN')
+        .then(response => {
+            console.log(response);
+            return response;
+        })
+        .catch(error => {
+            console.error(error);
+        });
 };
 
 export default getExchangeRates;
--- src/useConverter.js
+++ src/useConverter.js
@@ -3,12 +3,13 @@ import getExchangeRates from './getExchangeRates';
 
 const useConverter = (currency, currencyAmount) => {
     const [conversionResult, setConversionResult] = useState("");
-    const exchangeRates = {USD: 4, EUR: 3, GBP:2};
 
     useEffect(() => {
-        const exchangeRate = exchangeRates[currency];
-        const result = (currencyAmount * exchangeRate).toFixed(2);
-        setConversionResult(result);
+        getExchangeRates().then(exchangeRates => {
+            const exchangeRate = exchangeRates.data.data[currency].value;
+            const result = (currencyAmount * exchangeRate).toFixed(2);
+            setConversionResult(result);
+        });
     }, [currencyAmount, currency]);
 
     return conversionResult;
