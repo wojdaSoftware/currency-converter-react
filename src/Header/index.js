cbaebf370763b183f8740a0d9dd3c7925ececfff
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-04 22:33:01 +0100

    temporarily removed the date

--- src/Header/index.js
+++ src/Header/index.js
@@ -2,14 +2,6 @@ import { StyledHeader, Subtitle, Title } from './styled.js';
 import { useState, useEffect } from 'react';
 import useExchangeRates from '../useExchangeRates';
 
-const formatDate = (date) => {
-    return date.toLocaleString("pl-PL", {
-        day: "numeric",
-        month: "numeric",
-        year: "numeric",
-    });
-};
-
 const Header = ({ aboveTitleContent }) => (
     <StyledHeader>
         {aboveTitleContent}
