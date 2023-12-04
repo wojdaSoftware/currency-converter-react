cfd2baf299230a6bf3223c70e12fd383504a902b
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-04 19:52:45 +0100

    prevent fetching API data everytime hook is called

--- src/useConverter.js
+++ src/useConverter.js
@@ -3,14 +3,21 @@ import getExchangeRates from './getExchangeRates';
 
 const useConverter = (currency, currencyAmount) => {
     const [conversionResult, setConversionResult] = useState("");
+    const [exchangeRates, setExchangeRates] = useState(null);
 
     useEffect(() => {
-        getExchangeRates().then(exchangeRates => {
+        getExchangeRates().then(data => {
+            setExchangeRates(data);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (exchangeRates) {
             const exchangeRate = exchangeRates.data.data[currency].value;
             const result = (currencyAmount * exchangeRate).toFixed(2);
             setConversionResult(result);
-        });
-    }, [currencyAmount, currency]);
+        }
+    }, [currencyAmount, currency, exchangeRates]);
 
     return conversionResult;
 }
