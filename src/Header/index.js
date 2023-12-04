0e5343a5948accce9872b404731367cd9ef4e7da
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-04 21:19:29 +0100

    shows date of API fetch

--- src/Header/index.js
+++ src/Header/index.js
@@ -1,10 +1,40 @@
 import { StyledHeader, Subtitle, Title } from './styled.js';
+import { useState, useEffect } from 'react';
+import getExchangeRates from '../getExchangeRates';
+
+const useFetchDate = () => {
+    const [exchangeRates, setExchangeRates] = useState(null);
+    const [fetchDate, setFetchDate] = useState(null);
+
+    useEffect(() => {
+        getExchangeRates().then(data => {
+            setExchangeRates(data);
+        });
+    }, []);
+
+    useEffect(() => {
+        if (exchangeRates) {
+            const date = new Date(exchangeRates.meta.last_updated_at);
+            setFetchDate(formatDate(date));
+        }
+    }, [exchangeRates]);
+
+    return fetchDate;
+}
+
+const formatDate = (date) => {
+    return date.toLocaleString("pl-PL", {
+        day: "numeric",
+        month: "numeric",
+        year: "numeric",
+    });
+};
 
 const Header = ({ aboveTitleContent }) => (
     <StyledHeader>
         {aboveTitleContent}
         <Title>$ KALKULATOR WALUT $</Title>
-        <Subtitle>Kursy walut z dnia 24 lipca 2023 roku</Subtitle>
+        <Subtitle>Kursy walut z dnia {useFetchDate()}</Subtitle>
     </StyledHeader>
 );
 
