3685d1e2255d82bd142f3158bcff8aca505516d1
Kacper Wojda <wojda.kacper@gmail.com>
2023-12-05 19:26:32 +0100

    moved URLs

--- src/useExchangeRates.js
+++ src/useExchangeRates.js
@@ -1,13 +1,18 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
+const exchangeRateURL = {
+    static: "https://raw.githubusercontent.com/Kvasir124/currency-converter-react/main/src/rates.json",
+    dynamic: "https://api.currencyapi.com/v3/latest?apikey=cur_live_rycdtBwznBtiZdlOVMz0OLXrcBYp1XIJV2EZYJhc&currencies=EUR%2CUSD%2CGBP&base_currency=PLN",
+}
+
 const useExchangeRates = () => {
     const [exchangeRates, setExchangeRates] = useState({ status: "pending", data: null });
 
     useEffect(() => {
         setTimeout(async () => {
             try {
-                const response = await axios.get("https://raw.githubusercontent.com/Kvasir124/currency-converter-react/main/src/rates.json");
+                const response = await axios.get(exchangeRateURL.static);
                 setExchangeRates({
                     status: "success",
                     data: response.data,
