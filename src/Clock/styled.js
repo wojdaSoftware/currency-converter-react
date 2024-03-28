568e2b55c242758b1c89ff012c38ab7d352df997
Kacper Wojda <wojda.kacper@gmail.com>
2024-03-28 16:30:39 +0100

    formatting

--- src/Clock/styled.js
+++ src/Clock/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ClockContainer = styled.div`
     font-family: 'Courier New', Courier, monospace;
-    color: ${({theme}) => theme.colors.secondary};
+    color: ${({ theme }) => theme.colors.secondary};
     text-align: right;
     padding: 15px;
     font-size: smaller;
